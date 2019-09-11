197fc2760e23a0e49973160f612578c37d6c8955
manojsraogithub <38416998+manojsraogithub@users.noreply.github.com>
2019-09-11 18:30:55 +0530

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,14 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_3CHG6xT69', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '38u2lf6csd41r9cf1264729pnt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://7t7pp64cbc.execute-api.ap-southeast-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'manoj.rao@powerupcloud.com'
+        email: 'hello@world.com'
     }
 };
 
